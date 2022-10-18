ea509b820618c62b5297463c371248b45dd8aa69
stahova <stahova2404@gmail.com>
2022-10-18 19:02:48 +0300

    add page

--- app/js/common.js
+++ app/js/common.js
@@ -1,102 +1,96 @@
-
-
-
 const sliderInsights = () =>{
     $('.insights__list').slick({
         dots:true,
         arrows : true,
         slidesToShow: 3,
-        slidesToScroll: 1, centerMode: true,
-        centerPadding: '10rem',
+        slidesToScroll: 1,
+        // centerMode: true,
+        // centerPadding: '10rem',
         prevArrow: '<button class="insights-prev"></button>',
         nextArrow: '<button class="insights-next"></button>'
     });
+    if ($(window).width() < 666) {
+        $(".insights__list").slick('slickSetOption', 'slidesToShow', 1);
+    }
 }
+
+const validateForm = () => {
+    $('#modal').fancybox();
+    $('.subs__form').validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 2
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            phone: {
+                required: true,
+                digits : true,
+            }
+        },
+        messages: {
+            name: {
+                required: "Please specify your name",
+                minlength: "Name can't be shorter than 2 characters"
+            },
+            email: {
+                required: "Please enter your email address",
+                email: "Please enter valid email address"
+            }
+        },
+        submitHandler: function () {
+            $("#modal").trigger('click');
+            $('.subs__form')[0].reset();
+        }
+    });
+}
+
+
+const accordion  = () =>{
+    $('.culture__item-toggle').click(function(e) {
+        e.preventDefault();
+        let $this = $(this);
+
+        if ($this.next().hasClass('show')) {
+            $this.next().removeClass('show');
+            $this.next().slideUp(400);
+        } else {
+            $this.parent().parent().find('li .culture__item-inner').removeClass('show');
+            $this.parent().parent().find('li .culture__item-inner').slideUp(400);
+            $this.next().toggleClass('show');
+            $this.next().slideToggle(0);
+        }
+    });
+}
+const openMenu  = () =>{
+    $(".header__burger").click(function() {
+        if ($("#header__burger-content").hasClass("header__burger-hidden")) {
+            $("#header__burger-content").attr("class", "header__burger-visible animated header__burger-menu");
+            // $('body').css('overflow','hidden')
+            $('.header__burger-wrap').css({'background-color':'white','position':'relative'})
+        } else {
+            $("#header__burger-content").attr("class", "header__burger-hidden animated");
+            // $('body').css('overflow','visible')
+            $('.header__burger-wrap').css({'background-color':'transparent','position':'absolute'})
+        }
+        $(this).toggleClass("header__burger-open");
+    });
+};
+
+
 $(document).ready(function(){
     sliderInsights();
 })
 
+$( window ).on( "load", function() {
+    accordion();
+    openMenu();
+    validateForm()
+});
 
 
 
 
-//
-// let $carousel = $('.insights__list');
-//
-// let settings = {
-//     dots: false,
-//     arrows: false,
-//     slide: '.insights__item',
-//     slidesToShow: 3,
-//     centerMode: true,
-//     centerPadding: '14rem',
-// };
-//
-// function setSlideVisibility() {
-//     //Find the visible slides i.e. where aria-hidden="false"
-//     let visibleSlides = $carousel.find('.insights__item[aria-hidden="false"]');
-//     //Make sure all of the visible slides have an opacity of 1
-//     $(visibleSlides).each(function() {
-//         $(this).css('opacity', 1);
-//     });
-//
-//     //Set the opacity of the first and last partial slides.
-//     $(visibleSlides).first().prev().css('opacity', 0);
-// }
-//
-// $carousel.slick(settings);
-// $carousel.slick('slickGoTo', 1);
-// setSlideVisibility();
-//
-// $carousel.on('afterChange', function() {
-//     setSlideVisibility();
-// });
-
-
-
-
-
-// const sliderBlog = () =>{
-//     if (window.innerWidth <= 666) {
-//         $(".blog__list").addClass('slider__blog');
-//         $('.slider__blog').slick({
-//             dots: true,
-//         });
-//     }
-// }
-// const openMenu = () => {
-//     $(".header__burger").toggleClass("header__burger-close");
-//     $(".header__menu").toggleClass("header__menu-open");
-//     $("body").toggleClass('no-scroll');
-// }
-// const validateForm = () => {
-//     $('#modal').fancybox();
-//     $('#signin__form').validate({
-//         rules: {
-//             email: {
-//                 required: true,
-//                 email: true
-//             }
-//         },
-//         messages: {
-//             email: {
-//                 required: "Please enter your email address."
-//             }
-//         },
-//         submitHandler: function (form) {
-//             $("#modal").trigger('click');
-//             $('#signin__form')[0].reset();
-//         }
-//     });
-//
-// }
-//
-// $(document).ready(function(){
-//     sliderReview();
-//     sliderBlog()
-// })
-//
-// $( window ).on( "load", function() {
-//     validateForm()
-//     $(".header__mob").on("click", openMenu);
-// });
